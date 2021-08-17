a75c65295ffe2ed869212507e094a718ce488f80
Jennifer Reif <jennifer.reif@neo4j.com>
2021-08-17 15:26:15 -0500

    Update Spring Boot and test class

--- src/test/java/com/jmhreif/mariacodewars/MariaCodeWarsApplicationTests.java
+++ src/test/java/com/jmhreif/mariacodewars/MariaCodeWarsApplicationTests.java
@@ -1,16 +1,13 @@
 package com.jmhreif.mariacodewars;
 
 import org.junit.Test;
-import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.junit4.SpringRunner;
 
-@RunWith(SpringRunner.class)
 @SpringBootTest
-public class MariaCodeWarsApplicationTests {
+class MariaCodeWarsApplicationTests {
 
 	@Test
-	public void contextLoads() {
+	void contextLoads() {
 	}
 
 }
